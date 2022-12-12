99e439fe4b9b1ef7ab35eeb1b9ac8ef57296cf97
J Palmer <j-mpalmer@live.com>
2022-12-12 12:48:42 -0400

    added line reading function to lib

--- src/read.rs
+++ src/read.rs
@@ -1,6 +1,19 @@
 use ::std::fmt::Debug;
 use std::{fs, path::PathBuf, str::FromStr};
 
+pub fn read_linedata_from_file<T: FromStr>(file: impl Into<PathBuf>) -> Vec<T>
+where
+    <T as FromStr>::Err: Debug,
+{
+    let content = fs::read_to_string(file.into()).expect("file not found");
+
+    content
+        .split("\n")
+        .filter(|l| !l.is_empty())
+        .map(|l| l.parse::<T>().unwrap())
+        .collect()
+}
+
 
 pub fn read_packets_from_file<T: FromStr>(file: impl Into<PathBuf>) -> Vec<Vec<T>>
 where
