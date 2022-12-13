7ff849a1a0079ebb163645810295291c0ca46570
J Palmer <j-mpalmer@live.com>
2022-12-13 11:43:25 -0400

    day 6 solution

--- src/bin/runaoc/aoc2022/aoc2022_06.rs
+++ src/bin/runaoc/aoc2022/aoc2022_06.rs
@@ -1,25 +1,59 @@
-pub struct Aoc2022_06;
+pub struct Aoc2022_06 {
+    buffer: Vec<char>,
+}
 
 impl Aoc2022_06 {
     pub fn new() -> Self {
-        Self
+        Self { buffer: vec![] }
     }
 }
 
 impl crate::Runner for Aoc2022_06 {
     fn name(&self) -> (usize, usize) {
-       (22, 06)
+        (2022, 06)
     }
 
     fn parse(&mut self) -> () {
-        ()
+        let file = "input/2022-06.txt";
+
+        let content = aoc::read_linedata_from_file::<String>(file);
+
+        self.buffer = content[0].as_str().chars().collect();
     }
 
     fn part1(&mut self) -> Vec<String> {
-         crate::output("unsolved")
-    }
+        for (idx, window) in self.buffer.as_slice().windows(4).enumerate() {
+            let mut seen = std::collections::HashSet::new();
 
+            for ch in window {
+                if !seen.insert(ch) {
+                    break;
+                }
+
+                if seen.len() == 4 {
+                    return crate::output(idx + 4);
+                }
+            }
+        }
+
+        crate::output("unreachable")
+    }
+    
     fn part2(&mut self) -> Vec<String> {
-        crate::output("unsolved")
+        for (idx, window) in self.buffer.as_slice().windows(14).enumerate() {
+            let mut seen = std::collections::HashSet::new();
+            
+            for ch in window {
+                if !seen.insert(ch) {
+                    break;
+                }
+                
+                if seen.len() == 14 {
+                    return crate::output(idx + 14);
+                }
+            }
+        }
+        
+        crate::output("unreachable")
     }
 }
