f45b329a8e11333373f17881fd9f48b8e812c6a4
J Palmer <j-mpalmer@live.com>
2022-12-18 10:42:52 -0400

    added read packets for test files (windows line endings

--- src/read.rs
+++ src/read.rs
@@ -45,3 +45,21 @@ where
         })
         .collect()
 }
+
+pub fn read_packets_from_test_file<T: FromStr>(file: impl Into<PathBuf>) -> Vec<Vec<T>>
+where
+    <T as FromStr>::Err: Debug,
+{
+    let content = fs::read_to_string(file.into()).expect("file not found");
+
+    content
+        .split("\r\n\r\n")
+        .filter(|p| !p.is_empty())
+        .map(|p| {
+            p.split("\r\n")
+                .filter(|s| !s.is_empty())
+                .map(|s| s.parse::<T>().unwrap())
+                .collect()
+        })
+        .collect()
+}
